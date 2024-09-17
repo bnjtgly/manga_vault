430db7f9c997def8ba50c6435713bd83425ccde8
Benjie <84297158+bnjtgly@users.noreply.github.com>
2024-09-17 12:23:31 +0800

    Add files via upload

---
+++ Persistence.swift
@@ -0,0 +1,23 @@
+//
+//  Persistence.swift
+//  manga_vault
+//
+//  Created by Benjie Tigley on 9/17/24.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "MangaVault")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
